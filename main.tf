c931c583e7ae82a97a81b823d5828b84379e0704
RamandeepMaan <119065521+RamandeepMaan@users.noreply.github.com>
2024-10-10 00:24:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ resource "aws_vpc" "main" {
   cidr_block = "192.168.1.0/24"
   
 }
+resource "aws_vpc" "main" {
+  cidr_block = "192.168.2.0/24"
+ }
+
 output "name" {
   value = aws_vpc.main.cidr_block
 }
